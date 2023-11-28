baf8bba95336838a0b407aef132d5f88b0a5a15d
Saturn <25212998+Shadow12587@users.noreply.github.com>
2023-11-28 14:19:08 -0500

    Added JSON request to server for both login and sign up

--- app/src/main/java/com/example/treasurefinder/SignUp.java
+++ app/src/main/java/com/example/treasurefinder/SignUp.java
@@ -3,12 +3,65 @@ package com.example.treasurefinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SignUp extends AppCompatActivity {
+
+    TextView txtUsername;
+    TextView txtPassword;
+
+    TextView txtPasswordConfirmation;
+
+    RequestQueue queue;
+
+    String URL = "";
+
+    Boolean login;
+    String serverResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        txtUsername = findViewById(R.id.txtCreateUsername);
+        txtPassword = findViewById(R.id.txtCreatePassword);
+        txtPasswordConfirmation = findViewById(R.id.txtConfirmPassword);
+
+        queue = Volley.newRequestQueue(this);
+    }
+
+    public void createAccount() throws JSONException {
+        //Send to server here
+        //Send hashed username, password, and salt used for hashing
+
+        String username = (String) txtUsername.getText();
+        String password = (String) txtPassword.getText();
+        String confirmPassword = (String) txtPasswordConfirmation.getText();
+
+        if(confirmPassword.equals(password)) {
+            JSONObject j = new JSONObject();
+            j.put("Username", username);
+            j.put("Password", password);
+
+            JsonObjectRequest r = new JsonObjectRequest(Request.Method.POST, URL, j, response -> {
+                //Add code for response here, in theory server should respond with a t/f depending on if login worked
+            }, error -> {
+            });
+        }
+
+        else {
+            Toast.makeText(this, "ERROR: PASSWORD CONFIRMATION DOES NOT MATCH", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
