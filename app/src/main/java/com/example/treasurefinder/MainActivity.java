a03d509d7aab407b9f11fad2dc6e66c7b6cc193b
Saturn <25212998+Shadow12587@users.noreply.github.com>
2023-12-03 18:19:15 -0500

    Added comments and extra toasts in case of JSON error

--- app/src/main/java/com/example/treasurefinder/MainActivity.java
+++ app/src/main/java/com/example/treasurefinder/MainActivity.java
@@ -18,12 +18,20 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    //TextView for username entry
     TextView txtUsername;
+
+    //TextView for password entry
     TextView txtPassword;
 
+    //Queue for sending JSON requests
     RequestQueue queue;
 
+    //URL for login
     String URL = "https://treasurefinderbackend.onrender.com/login";
+
+    //String for server response during login
     String serverResponse;
 
     @Override
@@ -31,49 +39,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //sets username and password textviews to their respective views
         txtUsername = findViewById(R.id.txtUsername);
         txtPassword = findViewById(R.id.txtPassword);
 
+        //Instantiates queue
         queue = Volley.newRequestQueue(this);
     }
 
     public void login(View v) throws JSONException {
 
-        //Send to server here
-        //Send hashed username, password, and salt used for hashing
-
+        //Creates a new string from the username entered into the username text view
         String username = txtUsername.getText().toString();
+
+        //Creates a new string from the password entered into the password text view
         String password = txtPassword.getText().toString();
 
+        //Creates a new JSON Object
         JSONObject j = new JSONObject();
+
+        //Adds the username and password into the object
         j.put("username", username);
         j.put("password", password);
 
+        //Creates a new JSON request to send the username and password over to the server for authentication
         JsonObjectRequest r = new JsonObjectRequest(Request.Method.GET, URL, j, response -> {
-            //Add code for response here, in theory server should respond with an ID if login is valid
             try {
+                //serverResponse string is set to the respone held in the ID tag of the JSON response
                 serverResponse = response.get("id").toString();
             } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
 
+            //If statement checks to see if the serverResponse says error
             if (serverResponse.equals("ERROR")) {
+
+                //If so, show toast stating login info was wrong
                 Toast.makeText(this, "ERROR: LOGIN INFORMATION WRONG", Toast.LENGTH_SHORT).show();
             }
 
+            //If not
             else {
+
+                //Make a new intent for the salesActivity, add the id information and launch the intent
                 Intent i = new Intent(this, SalesActivity.class);
                 i.putExtra("ID", serverResponse);
                 startActivity(i);
             }
 
         }, error -> {
+            //If theres a JSON error, show toast stating server error
+            Toast.makeText(this, "ERROR: SERVER ERROR", Toast.LENGTH_SHORT).show();
         });
 
+        //Add JSON request to queue
         queue.add(r);
     }
 
     public void signUp(View v) {
+        //Launch SignUp activity
         Intent i = new Intent(this, SignUp.class);
         startActivity(i);
     }
--- app/src/main/java/com/example/treasurefinder/SignUp.java
+++ app/src/main/java/com/example/treasurefinder/SignUp.java
@@ -17,13 +17,19 @@ import org.json.JSONObject;
 
 public class SignUp extends AppCompatActivity {
 
+    //TextView for username entry
     TextView txtUsername;
+
+    //TextView for password entry
     TextView txtPassword;
 
+    //TextView for password confirmation entry
     TextView txtPasswordConfirmation;
 
+    //Queue for sending JSON requests
     RequestQueue queue;
 
+    //URL for register
     String URL = "https://treasurefinderbackend.onrender.com/users/register";
 
     @Override
@@ -31,36 +37,47 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        //sets username, password, and password confirmation textviews to their respective views
         txtUsername = findViewById(R.id.txtCreateUsername);
         txtPassword = findViewById(R.id.txtCreatePassword);
         txtPasswordConfirmation = findViewById(R.id.txtConfirmPassword);
 
+        //Instantiates queue
         queue = Volley.newRequestQueue(this);
     }
 
     public void createAccount(View v) throws JSONException {
-        //Send to server here
-        //Send hashed username, password, and salt used for hashing
-
+        //Creates strings for username, password and password confirmation from the text in their respective text views
         String username = txtUsername.getText().toString();
         String password = txtPassword.getText().toString();
         String confirmPassword = txtPasswordConfirmation.getText().toString();
 
+        //If password in confirm password matches password in password text view
         if(confirmPassword.equals(password)) {
+
+            //Create new JSON object
             JSONObject j = new JSONObject();
+
+            //Add username and password to the object
             j.put("username", username);
             j.put("password", password);
 
+            //Creates a new JSON request to send the username and password over to the server for sign up
             JsonObjectRequest r = new JsonObjectRequest(Request.Method.POST, URL, j, response -> {
+                //On response from server, show toast saying their account was made and close activity
                 Toast.makeText(this, "ACCOUNT MADE!", Toast.LENGTH_SHORT).show();
                 finish();
             }, error -> {
+                //If theres a JSON error, show toast stating server error
+                Toast.makeText(this, "ERROR: SERVER ERROR", Toast.LENGTH_SHORT).show();
             });
 
+            //Add JSON request to queue
             queue.add(r);
         }
 
         else {
+            //If password and password confirmation do not match show toast saying passwords do not match
             Toast.makeText(this, "ERROR: PASSWORD CONFIRMATION DOES NOT MATCH", Toast.LENGTH_SHORT).show();
         }
 
