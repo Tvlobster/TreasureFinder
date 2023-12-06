6476ea4abf7041815627f5543ece1bf455be258a
Saturn <25212998+Shadow12587@users.noreply.github.com>
2023-12-06 13:13:57 -0500

    Attempting to show multiple permission requests

--- app/src/main/java/com/example/treasurefinder/MainActivity.java
+++ app/src/main/java/com/example/treasurefinder/MainActivity.java
@@ -96,7 +96,8 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        checkPermissions();
+        checkNotificationPermissions();
+        checkLocationPermissions();
     }
 
     public void login(View v) throws JSONException {
@@ -163,20 +164,20 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-    public void checkPermissions() {
+    public void checkNotificationPermissions() {
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
             Log.d(TAG, "Permissions NOT granted, requesting....");
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, NOTIFICATION_REQUEST_CODE);
         } else {
             Log.d(TAG, "Permissions already granted");
         }
+    }
 
+    public void checkLocationPermissions() {
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
         } else {
             Log.d(TAG, "Location access permitted...");
         }
-
-
     }
 }
